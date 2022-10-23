2cf2dd236f18160f7803c0b5ac6d44ca2cfecae8
willbrid <ngaswilly77@gmail.com>
2022-10-23 18:13:13 +0100

    feat: update

--- testscript.sh
+++ testscript.sh
@@ -2,4 +2,4 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "This was added for testing github webhook with jenkins"
+echo "This was added for testing github webhook with jenkins 2"
